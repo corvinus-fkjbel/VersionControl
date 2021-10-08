3be3188132e8f800720ea6393d818bc8c2c23599
Dávid Szentesi <david.szentesi@stud.uni-corvinus.hu>
2021-10-08 10:48:06 +0200

    try catch

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -16,12 +16,17 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
 
             
         }
@@ -29,6 +34,34 @@ namespace ExcelExport
         public void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+            
+        }
+        private void CreateTable()
+        { 
+        
         }
 
     }
