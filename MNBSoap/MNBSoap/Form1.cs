b2fc4b0b3d1550e8963905f67cd8ebfb1a214150
Dávid Szentesi <david.szentesi@stud.uni-corvinus.hu>
2021-10-29 10:07:42 +0200

    Create webservice

---
+++ MNBSoap/MNBSoap/Form1.cs
@@ -0,0 +1,34 @@
+﻿using MNBSoap.MnbServicereference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MNBSoap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Consume();
+        }
+
+        void Consume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            mnbService.GetExchangeRates(request);
+
+
+        }
+    }
+}
