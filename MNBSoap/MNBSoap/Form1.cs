deb1484689329b4f6ed3526113351784b2e61de6
Dávid Szentesi <david.szentesi@stud.uni-corvinus.hu>
2021-10-29 10:36:37 +0200

    chart

--- MNBSoap/MNBSoap/Form1.cs
+++ MNBSoap/MNBSoap/Form1.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace MNBSoap
 {
@@ -18,10 +20,53 @@ namespace MNBSoap
         public Form1()
         {
             InitializeComponent();
-            Consume();
+            string xmlstring=Consume();
+            LoadXml(xmlstring);
+            dataGridView1.DataSource = Rates;
+            Charting();
+        }
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
-        void Consume()
+
+
+        private void LoadXml(string input)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(input);
+
+           
+            foreach (XmlElement element in xml.DocumentElement)
+            {              
+                var rate = new RateData();
+                Rates.Add(rate);             
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        string Consume()
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
@@ -31,7 +76,7 @@ namespace MNBSoap
             mnbService.GetExchangeRates(request);
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
-            FileDialog.WriteAllText("export.xml", result);
+            return result;
 
 
         }
