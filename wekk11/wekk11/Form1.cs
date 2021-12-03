6d8942bc0f70700509996ddec3b6112d3c3c8405
Dávid Szentesi <david.szentesi@stud.uni-corvinus.hu>
2021-12-03 16:24:30 +0100

    Felhasználói felulet

--- wekk11/wekk11/Form1.cs
+++ wekk11/wekk11/Form1.cs
@@ -142,5 +142,15 @@ namespace wekk11
 
             return population;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
