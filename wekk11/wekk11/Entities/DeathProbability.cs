77ed8626bf3f035e700b5f9ba94c2fe7a123f9f6
Dávid Szentesi <david.szentesi@stud.uni-corvinus.hu>
2021-12-03 10:39:53 +0100

    sr

---
+++ wekk11/wekk11/Entities/DeathProbability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wekk11.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+
+
+        
+    }
+}
